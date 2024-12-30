b7b8b129ea26a1ba1b78dfb9466d7f31b9b898ab
UIT.23520349 <23520349@gm.uit.edu.vn>
2024-12-31 01:33:37 +0700

    Finish password recovery

--- Qlyrapchieuphim/Datlaimk.cs
+++ Qlyrapchieuphim/Datlaimk.cs
@@ -18,10 +18,14 @@ namespace Qlyrapchieuphim
         public Datlaimk()
         {
             InitializeComponent();
+            passBox.UseSystemPasswordChar = true;
+            pass_repeatBox.UseSystemPasswordChar = true;
         }
         public Datlaimk(string mnv)
         {
             InitializeComponent();
+            passBox.UseSystemPasswordChar = true;
+            pass_repeatBox.UseSystemPasswordChar = true;
             manv = mnv;
         }
         string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
@@ -35,6 +39,8 @@ namespace Qlyrapchieuphim
 
         private void confirmButton_Click(object sender, EventArgs e)
         {
+            passBox.Text = passBox.Text.Trim();
+            pass_repeatBox.Text = pass_repeatBox.Text.Trim();
             if (passBox.Text != pass_repeatBox.Text)
             {
                 MessageBox.Show(
@@ -47,7 +53,35 @@ namespace Qlyrapchieuphim
             SqlConnection conn = new SqlConnection(ConnString);
             string SqlQuery = "UPDATE NHANVIEN SET " +
                 "PASS = @pass " +
-                "WHERE ()";
+                "WHERE (MANHANVIEN = @manv)";
+            SqlCommand cmd = new SqlCommand(SqlQuery, conn);
+            cmd.Parameters.Add("@pass",SqlDbType.VarChar).Value = passBox.Text.Trim();
+            cmd.Parameters.Add("@manv", SqlDbType.Char).Value = manv;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            DialogResult result = MessageBox.Show(
+                "Mật khẩu đã được đổi thành công!",
+                "Thông báo",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information);
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Close();
+        }
+
+        private void guna2CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (guna2CheckBox1.Checked)
+            {
+                passBox.UseSystemPasswordChar = false;
+                pass_repeatBox.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                passBox.UseSystemPasswordChar = true;
+                pass_repeatBox.UseSystemPasswordChar = true;
+            }
         }
     }
 }
